15f081443fe51d886e8c8aec04b8a9fa9327ae38
Micah Guttman <memtech3@gmail.com>
2022-04-07 04:12:06 -0500

    fixes some edge cases

--- src/main/java/frc/robot/commands/shooting/LinearInterpolation.java
+++ src/main/java/frc/robot/commands/shooting/LinearInterpolation.java
@@ -7,7 +7,7 @@ package frc.robot.commands.shooting;
 /** Add your docs here. */
 public class LinearInterpolation {
     public static double calculate(double[] xCoords, double[] yCoords, double value) {
-        int index = 1;
+        int index = xCoords.length-1;
 
         for (int i = 0; i < xCoords.length; i++) {
             if (xCoords[i] > value) {
@@ -18,10 +18,10 @@ public class LinearInterpolation {
 
         double slope = (yCoords[index] - yCoords[index - 1]) / (xCoords[index] - xCoords[index - 1]);
         double intercept = yCoords[index];
-        double deltaValue = xCoords[index] - value;
+        double deltaValue = Math.abs(xCoords[index] - value);
         System.out.println("Slope " + slope);
         System.out.println("Intercept " + intercept);
         System.out.println("Value" + value);
-        return deltaValue * slope + intercept;    
+        return deltaValue * slope + intercept;
     }
 }
