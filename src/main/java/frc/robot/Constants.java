da756b6af89e2c803750abb0c3726f04ff945016
Vincent Wren <wrenvin@gmail.com>
2022-04-08 20:22:13 -0500

    Lowered flywheel speed and kicker speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
 	public static final int[] FLYWHEEL_ENCODER_PORTS = new int[] { 4, 5 };
 	public static final double FLYWHEEL_ENCODER_CONVERSION_FACTOR = 1 / .4;
 	public static final int[] FLYWHEEL_MOTOR_IDS = new int[] { 9, 10 };
-	public static final double FLYWHEEL_SHOOTING_SPEED = 2280;
+	public static final double FLYWHEEL_SHOOTING_SPEED = 2260;
 	public static final double FLYWHEEL_SHOOTING_ANGLE = 36;
     public static final double[] FLYWHEEL_SPARKMAX_PIDF = new double[] { 0.00105, 8E-7, 0.000002, 0.00001 };
 
@@ -54,7 +54,7 @@ public final class Constants {
 	public static final double ACCUMULATOR_SPEED_MULTIPLIER = 1;
 	public static final int KICKER_SENSOR_PORT = 7;
 	public static final double KICKER_SPEED = .25;
-	public static final double KICKER_SHOOT_SPEED = 1;
+	public static final double KICKER_SHOOT_SPEED = .5;
 	// public static final double KICKER_SHOOT_SPEED = .25;
 
 	public static final double TOWER_SPEED = .5;
