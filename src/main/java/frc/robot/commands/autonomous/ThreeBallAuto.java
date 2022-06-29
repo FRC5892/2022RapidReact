a1311b8e365f83432b85ab1ff79ebf31a2c5c529
Vincent Wren <wrenvin@gmail.com>
2022-06-29 14:47:48 -0500

    Removed unused import

--- src/main/java/frc/robot/commands/autonomous/ThreeBallAuto.java
+++ src/main/java/frc/robot/commands/autonomous/ThreeBallAuto.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.commands.setIntakespeedManual;
 import frc.robot.commands.shooting.AimAndShoot;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.serializer.Intake;
